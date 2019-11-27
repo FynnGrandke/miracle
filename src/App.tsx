f9e0235ba70ce14dc331c573f6d3f651c30e2a3a
Fynn Grandke <fynn.grandke@web.de>
2019-11-27 22:15:12 +0900

    Added comment

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { GameObserver } from './components/GameObserver';
 import './css/main.css';
 
+// Entry point
 render(
   <GameObserver />,
   document.getElementById('root')
